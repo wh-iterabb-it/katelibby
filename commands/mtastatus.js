d6003f91416e0fa02dd9be63d6921d9437dd3cdc
beaubouchard <beau@beaubouchard.com>
2016-12-26 18:53:53 -0500

    fix: progressing some mtastatus updates, twtich client id

--- commands/mtastatus.js
+++ commands/mtastatus.js
@@ -4,6 +4,20 @@ import entities from 'entities';
 import irc from 'irc';
 
 const url = 'http://web.mta.info/status/serviceStatus.txt';
+const line_colors = {
+  '123': 'light_red',
+  '456': 'dark_green',
+  '7': 'magenta',
+  'ACE': 'dark_blue',
+  'BDFM': 'orange',
+  'G': 'light_green',
+  'JZ': 'dark_red',
+  'NQR': 'yellow',
+  'L': 'gray',
+  'S': 'gray',
+  'SIR': 'dark_blue',
+};
+
 
 module.exports = (callback, target, from, args) => {
   if (typeof args !== 'undefined') {
@@ -13,108 +27,91 @@ module.exports = (callback, target, from, args) => {
         callback.say(target, 'Syntax is ' + callback.config.commandChar +
          'mtastatus { line }');
         return 'help';
-       default:
+      default:
         callback.say(target, 'You must specify a line!');
     }
   }
 
-  const line_colors = {
-    '123':  'light_red',
-    '456':  'dark_green',
-    '7':    'magenta',
-    'ACE':  'dark_blue',
-    'BDFM': 'orange',
-    'G':    'light_green',
-    'JZ':   'dark_red',
-    'NQR':  'yellow',
-    'L':    'gray',
-    'S':    'gray',
-    'SIR':  'dark_blue',
+  // Make sure we asked for a sane line
+  if (line_key === null) {
+    callback.say(target, 'I do not know that line!');
   }
 
-    var line_key = getLineKey(args),
-        all_status = [];
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      parser.parseString(body, (err, data) => {
+        for (let i in data.service.subway[0].line) {
+          const line = data.service.subway[0].line[i];
+            name = line.name[0],
+            status = line.status[0],
+            text = sanitize(line.text[0]);
 
-    // Make sure we asked for a sane line
-    if (line_key == null) {
-        return "I don't know that line :/";
-    }
+          all_status[name] = {
+            status: status,
+            text: text,
+          };
+      }
 
-    request(url, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            parser.parseString(body, (err, data) => {
-                for (let i in data.service.subway[0].line) {
-                    const line = data.service.subway[0].line[i];
-                        name = line.name[0],
-                        status = line.status[0],
-                        text = sanitize(line.text[0]);
+       let theLine = all_status[line_key];
+       let color = getColorForLine(line_key);
+       let outstr = line_key + ": " + theLine.status + "\n" + theLine.text;
 
-                    all_status[name] = {
-                        status: status,
-                        text: text,
-                    };
-                }
-
-               let theLine = all_status[line_key];
-               let color = getColorForLine(line_key);
-               let outstr = line_key + ": " + theLine.status + "\n" + theLine.text;
-
-                if (color) {
-                    outstr = irc.colors.wrap(color, line_key) + ": " + theLine.status + "\n" + theLine.text;
-                }
-
-                callback.say(target, outstr);
-            });
+        if (color) {
+            outstr = irc.colors.wrap(color, line_key) + ": " + theLine.status + "\n" + theLine.text;
         }
-    });
+
+        callback.say(target, outstr);
+      });
+    }
+  });
 }
 
 function getLineKey(input) {
-    switch (input.toUpperCase()) {
-        case '1':
-        case '2':
-        case '3':
-            return '123';
-        case '4':
-        case '5':
-        case '6':
-            return '456';
-        case '7':
-            return '7';
-        case 'A':
-        case 'C':
-        case 'E':
-            return 'ACE';
-        case 'B':
-        case 'D':
-        case 'F':
-        case 'M':
-            return 'BDFM';
-        case 'G':
-            return 'G';
-        case 'J':
-        case 'Z':
-            return 'JZ';
-        case 'L':
-            return 'L';
-        case 'N':
-        case 'Q':
-        case 'R':
-            return 'NQR';
-        case 'S':
-            return 'S';
-        case 'SIR':
-            return 'SIR';
-        default:
-            return null;
-    }
+  switch (input.toUpperCase()) {
+    case '1':
+    case '2':
+    case '3':
+      return '123';
+    case '4':
+    case '5':
+    case '6':
+      return '456';
+    case '7':
+      return '7';
+    case 'A':
+    case 'C':
+    case 'E':
+      return 'ACE';
+    case 'B':
+    case 'D':
+    case 'F':
+    case 'M':
+      return 'BDFM';
+    case 'G':
+      return 'G';
+    case 'J':
+    case 'Z':
+      return 'JZ';
+    case 'L':
+      return 'L';
+    case 'N':
+    case 'Q':
+    case 'R':
+      return 'NQR';
+    case 'S':
+      return 'S';
+    case 'SIR':
+      return 'SIR';
+    default:
+      return null;
+  }
 }
 
 function getColorForLine(line) {
-    if (typeof line_colors[line] != 'undefined') {
-        return line_colors[line];
-    }
-    return null;
+  if (typeof line_colors[line] !== 'undefined') {
+    return line_colors[line];
+  }
+  return null;
 }
 
 function sanitize(text) {
--- commands/twitch.js
+++ commands/twitch.js
@@ -1,3 +1,5 @@
+import request from 'request';
+
 module.exports = (callback, target, from, args) => {
   if (typeof args !== 'undefined') {
     switch (args) {
@@ -7,9 +9,25 @@ module.exports = (callback, target, from, args) => {
         return 'help';
     }
   }
-  const status = 'live';
-  callback.say(target, 'https://twitch.com/' + callback.config.twitch.streamer + ' is currently ' + status);
-  callback.say(target, 'Also so make sure to check out ');
+  const api_url = 'https://api.twitch.tv/kraken/streams/';
+  const options = {
+    url: api_url + callback.config.twitch.streamer,
+    headers: {
+      'Client-ID': callback.config.twitch.clientid,
+    },
+  };
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const json = JSON.parse(body);
+      if (typeof json.current_observation === 'undefined') {
+        callback.say(target, 'Are you trying to make me crash?');
+      } else {
+        const returnstring = '';
+        callback.say(target, 'https://twitch.com/' + callback.config.twitch.streamer + ' is currently ' + returnstring);
+      }
+    }
+  });
+  callback.say(target, 'Also make sure to check out ');
   callback.config.twitch.personal.forEach((site) => {
     callback.say(target, site);
   });
