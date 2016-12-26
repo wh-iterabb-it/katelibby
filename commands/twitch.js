a00cf865342bbc61fa9f7480b4a9cbae825d4cbc
beaubouchard <beau@beaubouchard.com>
2016-12-26 14:40:25 -0500

    add: eslintrc, editorconfig, twitch

--- commands/twitch.js
+++ commands/twitch.js
@@ -9,4 +9,8 @@ module.exports = (callback, target, from, args) => {
   }
   const status = 'live';
   callback.say(target, 'https://twitch.com/'+ callback.config.twitch.streamer + ' is currently '+ status);
+  callback.say(target, 'Also so make sure to check out ');
+  for (let site in callback.config.twitch.personal) {
+      callback.say(target, callback.config.twitch.personal[site]);
+  }
 };
