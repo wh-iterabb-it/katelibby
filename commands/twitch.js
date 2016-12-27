759ec2c9002531e1d3ad5a1d18e9bb313fdc7d15
beaubouchard <beau@beaubouchard.com>
2016-12-27 00:09:13 -0500

    small update

--- commands/twitch.js
+++ commands/twitch.js
@@ -8,27 +8,23 @@ module.exports = (callback, target, from, args) => {
         callback.say(target, 'Syntax is ' + callback.config.commandChar + 'twitch');
         return 'help';
     }
-  }
-  const api_url = 'https://api.twitch.tv/kraken/streams/';
-  const options = {
-    url: api_url + callback.config.twitch.streamer,
-    headers: {
-      'Client-ID': callback.config.twitch.clientid,
-    },
-  };
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const json = JSON.parse(body);
-      if (typeof json.current_observation === 'undefined') {
-        callback.say(target, 'Are you trying to make me crash?');
-      } else {
-        const returnstring = '';
-        callback.say(target, 'https://twitch.com/' + callback.config.twitch.streamer + ' is currently ' + returnstring);
+    const api_url = 'https://api.twitch.tv/kraken/streams/';
+    const options = {
+      url: api_url + callback.config.twitch.streamer,
+      headers: {
+        'Client-ID': callback.config.twitch.clientid,
+      },
+    };
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const json = JSON.parse(body);
+        if (typeof json.current_observation === 'undefined') {
+          callback.say(target, 'Are you trying to make me crash?');
+        } else {
+          const returnstring = '';
+          callback.say(target, 'https://twitch.com/' + callback.config.twitch.streamer + ' is currently ' + returnstring);
+        }
       }
-    }
-  });
-  callback.say(target, 'Also make sure to check out ');
-  callback.config.twitch.personal.forEach((site) => {
-    callback.say(target, site);
-  });
+    });
+  }
 };
