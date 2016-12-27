9564b8cfb5dc19db419313801f64cfc7c209a740
beaubouchard <beau@beaubouchard.com>
2016-12-26 21:41:37 -0500

    added: uptime command

--- commands/mtastatus.js
+++ commands/mtastatus.js
@@ -17,55 +17,6 @@ const line_colors = {
   'S': 'gray',
   'SIR': 'dark_blue',
 };
-
-
-module.exports = (callback, target, from, args) => {
-  if (typeof args !== 'undefined') {
-    switch (args) {
-      case 'help':
-        callback.say(target, 'Check the status of your NYC Metro Line!');
-        callback.say(target, 'Syntax is ' + callback.config.commandChar +
-         'mtastatus { line }');
-        return 'help';
-      default:
-        callback.say(target, 'You must specify a line!');
-    }
-  }
-
-  // Make sure we asked for a sane line
-  if (line_key === null) {
-    callback.say(target, 'I do not know that line!');
-  }
-
-  request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      parser.parseString(body, (err, data) => {
-        for (let i in data.service.subway[0].line) {
-          const line = data.service.subway[0].line[i];
-            name = line.name[0],
-            status = line.status[0],
-            text = sanitize(line.text[0]);
-
-          all_status[name] = {
-            status: status,
-            text: text,
-          };
-      }
-
-       let theLine = all_status[line_key];
-       let color = getColorForLine(line_key);
-       let outstr = line_key + ": " + theLine.status + "\n" + theLine.text;
-
-        if (color) {
-            outstr = irc.colors.wrap(color, line_key) + ": " + theLine.status + "\n" + theLine.text;
-        }
-
-        callback.say(target, outstr);
-      });
-    }
-  });
-}
-
 function getLineKey(input) {
   switch (input.toUpperCase()) {
     case '1':
@@ -107,6 +58,55 @@ function getLineKey(input) {
   }
 }
 
+module.exports = (callback, target, from, args) => {
+
+  if (typeof args !== 'undefined') {
+    switch (args) {
+      case 'help':
+        callback.say(target, 'Check the status of your NYC Metro Line!');
+        callback.say(target, 'Syntax is ' + callback.config.commandChar +
+         'mtastatus { line }');
+        return 'help';
+      default:
+        if (getLineKey(args)) {
+          getLineKey(args);
+        } else {
+          callback.say(target, 'You must specify a valid line!');
+        }
+    }
+  }
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      parser.parseString(body, (err, data) => {
+        for (let i in data.service.subway[0].line) {
+          const line = data.service.subway[0].line[i];
+            name = line.name[0],
+            status = line.status[0],
+            text = sanitize(line.text[0]);
+
+          all_status[name] = {
+            status: status,
+            text: text,
+          };
+      }
+
+       let theLine = all_status[line_key];
+       let color = getColorForLine(line_key);
+       let outstr = line_key + ": " + theLine.status + "\n" + theLine.text;
+
+        if (color) {
+            outstr = irc.colors.wrap(color, line_key) + ": " + theLine.status + "\n" + theLine.text;
+        }
+
+        callback.say(target, outstr);
+      });
+    }
+  });
+}
+
+
+
 function getColorForLine(line) {
   if (typeof line_colors[line] !== 'undefined') {
     return line_colors[line];
@@ -115,16 +115,16 @@ function getColorForLine(line) {
 }
 
 function sanitize(text) {
-    text = text.replace(/<\/?(br *\/?)>/gi, '\r\n');
-    text = text.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, '');
-    text = entities.decodeHTML(text);
-    lines = text.split("\r\n");
-    for (let l in lines) {
-        lines[l] = lines[l].trim();
-    }
-    lines = lines.filter(function(value) {
-        return !(value == '' || value == null)
-    });
-    text = lines.join("\n");
-    return text;
+  text = text.replace(/<\/?(br *\/?)>/gi, '\r\n');
+  text = text.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, '');
+  text = entities.decodeHTML(text);
+  lines = text.split("\r\n");
+  for (let l in lines) {
+    lines[l] = lines[l].trim();
+  }
+  lines = lines.filter(function(value) {
+    return !(value == '' || value == null)
+  });
+  text = lines.join("\n");
+  return text;
 }
