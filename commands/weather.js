459cc10c139c62a4925503fe35fd73a7515aec09
Beau Bouchard <bnb2011@gmail.com>
2017-03-24 13:32:21 -0400

    adding default behavior without api key for weather

--- commands/weather.js
+++ commands/weather.js
@@ -10,25 +10,28 @@ module.exports = (callback, target, from, args) => {
         return 'help';
     }
   }
-  const api_key = callback.config.wunderground.key;
-  const api_url = 'http://api.wunderground.com/api/' +
-    api_key + '/conditions/q/';
-  const url = api_url + args + '.json';
-  request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const json = JSON.parse(body);
-      if (typeof json.current_observation === 'undefined') {
-        callback.say(target, 'Are you trying to make me crash?');
-      } else {
-        const returnstring = 'Current temperature in ' +
-        json.current_observation.display_location.city +
-        ', ' + json.current_observation.display_location.state_name +
-        ', ' + json.current_observation.display_location.zip +
-        ' is ' + json.current_observation.temp_f +
-        '°F, with a humidity of ' + json.current_observation.relative_humidity;
-        callback.say(target, returnstring);
+  if (callback.config.wunderground.key.length < 1) {
+    callback.say(target, 'Please add an API key to the configuration file.');
+  } else {
+    const api_key = callback.config.wunderground.key;
+    const api_url = 'http://api.wunderground.com/api/' +
+      api_key + '/conditions/q/';
+    const url = api_url + args + '.json';
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const json = JSON.parse(body);
+        if (typeof json.current_observation === 'undefined') {
+          callback.say(target, 'Are you trying to make me crash?');
+        } else {
+          const returnstring = 'Current temperature in ' +
+          json.current_observation.display_location.city +
+          ', ' + json.current_observation.display_location.state_name +
+          ', ' + json.current_observation.display_location.zip +
+          ' is ' + json.current_observation.temp_f +
+          '°F, with a humidity of ' + json.current_observation.relative_humidity;
+          callback.say(target, returnstring);
+        }
       }
-    }
-  });
-  // callback.say(target, 'searching ...');
+    });
+  }
 };
