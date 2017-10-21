e4f32cfcf3366a10ff78272fc9479fb58189fe00
Beau Bouchard <bnb2011@gmail.com>
2017-10-21 19:21:32 -0400

    feat: not sure if this is funny or silly

--- commands/optional/faq.js
+++ commands/optional/faq.js
@@ -2,12 +2,25 @@ module.exports = (callback, target, from, args) => {
   if (typeof args !== 'undefined') {
     switch (args) {
       case 'help':
-        callback.say(target, '8ball command is just for asking the magic RNG a question');
-        callback.say(target, 'Syntax is ' + callback.config.commandChar +
-         '8ball { question you want to ask }');
+        callback.say(target, 'FAQ command is just for answering commonly asked questions rudely');
+        callback.say(target, 'Syntax is ' + callback.config.commandChar + 'faq { subject }');
+        callback.say(target, 'The subjects are: ');
+        callback.say(target, '    { release } - what is the current release branch of the project.');
+        callback.say(target, '    { ooo } - the out of office email address for google calendar.');
+        callback.say(target, '    { deploy } - lets you know when the next release branch will be deployed.');
         return 'help';
     }
   }
-
-  callback.say(target, "faq")]);
+  /* 
+   * FAQ
+   *
+   */
+  const li = [
+    'The next release will be deployed later',
+    'We plan on releasing the next release soon',
+    'The next release comes out at exactly {{ DATABASE ERROR TABLE RELEASE TIME NOT FOUND }}',
+    'Remember good code can not be rushed, next release will be soon',
+  ];
+  callback.say(target, li[Math.floor(Math.random() * li.length)]);
+  callback.say(target, 'faq')]);
 };
