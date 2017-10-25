3f3ff4402748ae3d1d0c443fb3eaff65f0c5aa49
Beau Bouchard <bnb2011@gmail.com>
2017-10-24 22:02:19 -0400

    fix: added resolve and case for deploy, ooo

--- commands/optional/faq.js
+++ commands/optional/faq.js
@@ -2,6 +2,8 @@ import githubAPI from 'github';
 
 import config from '../helpers/config_helper';
 
+const ooo_msg = 'http://';
+
 module.exports = (callback, target, from, args) => {
   if (typeof args !== 'undefined') {
     switch (args) {
@@ -15,13 +17,13 @@ module.exports = (callback, target, from, args) => {
         return 'help';
       case 'release':
         // Authenticate with Github
-        let instance = new githubAPI( ...config.github.connect );
+        let instance = new githubAPI(...config.github.connect);
         instance.authenticate({
           type: config.github.auth.type,
           token: config.github.auth.token
         });
 
-        var currentRelease = new Promise((resolve, reject) => {
+        let currentRelease = new Promise((resolve, reject) => {
           instance.repos.getBranch({
             owner: config.github.owner,
             repo: config.github.repo,
@@ -30,25 +32,27 @@ module.exports = (callback, target, from, args) => {
             if (!err) {
               logger.info('request complete, return');
               callback.say(target, res.data.commit.message);
+              resolve(0);
               return 'release';
             } else {
-              logger.info( `get currentRelease has errored: ${err.toJSON()}`);
+              logger.info(`get currentRelease has errored: ${err.toJSON()}`);
+              resolve(1);
               return 'release';
             }
           });
         });
+        return 'release';
+      case 'ooo':
+        callback.say(target, ooo_msg);
+        return 'ooo';
+      case 'deploy':
+        const li = [
+          'The next release will be deployed later',
+          'We plan on releasing the next release soon',
+          'The next release comes out at exactly {{ DATABASE ERROR TABLE RELEASE TIME NOT FOUND }}',
+        ];
+        callback.say(target, li[Math.floor(Math.random() * li.length)]);
+        return 'deploy';
     }
   }
-
-  /*
-   * FAQ
-   *
-   */
-  const li = [
-    'The next release will be deployed later',
-    'We plan on releasing the next release soon',
-    'The next release comes out at exactly {{ DATABASE ERROR TABLE RELEASE TIME NOT FOUND }}',
-  ];
-  callback.say(target, li[Math.floor(Math.random() * li.length)]);
-  callback.say(target, 'faq')]);
 };
