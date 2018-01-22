1a40758f8809057e1db0c87fc14d187f93688a5c
beaubouchard <beau@beaubouchard.com>
2018-01-22 15:34:00 -0500

    chore: wrote tests for sanitize

---
+++ test/utils/sanitize.test.js
@@ -0,0 +1,48 @@
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+
+import { sanitize } from '../../utils/sanitize';
+
+chai.should();
+
+chai.use(sinonChai);
+
+const { expect } = chai;
+
+describe('Sanitize Utility Tests', () => {
+  describe('sanitize method', () => {
+    it('should return empty string if nothing is passed in', () => {
+      const expected = '';
+      const resp = sanitize();
+      expect(resp).to.equal(expected);
+    });
+
+    it('should return empty string if empty object is passed in', () => {
+      const testData = {};
+      const expected = '';
+      const resp = sanitize(testData);
+      expect(resp).to.equal(expected);
+    });
+
+    it('should remove br tags', () => {
+      const testData = '</br>';
+      const expected = '';
+      const resp = sanitize(testData);
+      expect(resp).to.equal(expected);
+    });
+
+    it('should remove br tag and add rn line', () => {
+      const testData = 'tacos</br>tacos';
+      const expected = 'tacos\ntacos';
+      const resp = sanitize(testData);
+      expect(resp).to.equal(expected);
+    });
+
+    it('should remove empty lines', () => {
+      const testData = 'tacos</br></br></br>tacos';
+      const expected = 'tacos\ntacos';
+      const resp = sanitize(testData);
+      expect(resp).to.equal(expected);
+    });
+  });
+});
--- utils/sanitize.js
+++ utils/sanitize.js
@@ -1,17 +1,5 @@
 import entities from 'entities';
 
-export function toHHMMSS(inctime) {
-  const secNum = parseInt(inctime, 10);
-  let hours = Math.floor(secNum / 3600);
-  let minutes = Math.floor((secNum - (hours * 3600)) / 60);
-  let seconds = secNum - (hours * 3600) - (minutes * 60);
-  if (hours < 10) { hours = '0' + hours; }
-  if (minutes < 10) { minutes = '0' + minutes; }
-  if (seconds < 10) { seconds = '0' + seconds; }
-  const time = hours + ':' + minutes + ':' + seconds;
-  return time;
-}
-
 export function sanitize(data) {
   let clean;
 
