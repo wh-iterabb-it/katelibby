aaea986e94072f80978b2c7ec0cb9da6ed7956c2
Beau Bouchard <bnb2011@gmail.com>
2018-01-21 20:41:00 -0500

    feat: added tests (#73)
    * feat: added tests
    
    * fix: touchups to the irc_helper to make it more testable
    
    * chore: minimal
    
    * chore: added some updates

--- test/helpers/irc_helper.test.js
+++ test/helpers/irc_helper.test.js
@@ -9,6 +9,8 @@ chai.should();
 
 chai.use(sinonChai);
 
+const expect = chai.expect;
+
 describe('IRC Helper', () => {
   describe('detectSlack method', () => {
     it('should return true when slack.com is in string', () => {
@@ -21,4 +23,54 @@ describe('IRC Helper', () => {
       expect(resp).be.false;
     });
   });
+
+  describe('detectTwitch method', () => {
+    it('should return true when twitch.tv is in string', () => {
+      const resp = irc.detectTwitch('blahblah.twitch.tv');
+      expect(resp).be.true;
+    });
+
+    it('should return false when twitch.tv is not in string', () => {
+      const resp = irc.detectTwitch('blahblah.asf.com');
+      expect(resp).be.false;
+    });
+  });
+
+  describe('isSUB method', () => {
+    it('should return false when a string is not a subreddit', () => {
+      const testString = "http://reddit.com/u/tacos";
+      const resp = irc.isSUB(testString);
+      expect(resp).be.false;
+    });
+
+    it('should return false when nothing or empty string is passed in', () => {
+      const testString = "";
+      const resp = irc.isSUB(testString);
+      expect(resp).be.false;
+    });
+
+    it('should return a subreddit when valid regex is passed in', () => {
+      const testString = "http://reddit.com/r/me_irl";
+      const expected = "/r/me_irl";
+      const resp = irc.isSUB(testString);
+      expect(resp).to.equal(expected);
+    });
+  });
+
+  describe('isURL method', () => {
+    it('should return false if not a valid url', () => {
+      const testString = "Remember that website? something .com or www. http:// something?";
+      const resp = irc.isURL(testString);
+      expect(resp).be.false;
+    });
+
+    it('should return a url if it is a valid url', () => {
+      const testString = "You guys talking about http://www.google.com ?";
+      const expected = "http://www.google.com";
+      const resp = irc.isURL(testString);
+      expect(resp).to.equal(expected);
+    });
+  });
+
+
 });
