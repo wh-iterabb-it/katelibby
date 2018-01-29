9d48069bb4d1728f412404a2acb8ae3401a3fc9c
Beau Bouchard <bnb2011@gmail.com>
2018-01-29 00:21:44 -0500

    fix: parsed int before tofixed

--- commands/coin.js
+++ commands/coin.js
@@ -27,10 +27,12 @@ module.exports = (callback, target, from, args) => {
           if (typeof body.Markets === 'undefined' || typeof body.error !== 'undefined') {
             callback.say(target, 'Are you trying to make me crash?');
           } else {
-            const price = body.Markets[0].Price.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            const priceInt = parseInt(body.Markets[0].Price);
+            const price =  priceInt.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
             const label = body.Markets[0].Label.substring(0, 3);
             const name = body.Markets[0].Name;
-            const volume = body.Markets[0].Volume_24h.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            const volumeInt = parseInt(body.Markets[0].Volume_24h);
+            const volume = volumeInt.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
             
             // get the time since last trade
             const timestamp = moment.unix(body.Markets[0].Timestamp).format('DD/MM/YYYY HH:mm:ss');
