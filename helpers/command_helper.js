96b8cd32970effde2e75cb0da65aa2be7f36abf6
Beau Bouchard <bnb2011@gmail.com>
2018-02-06 18:27:27 -0500

    feat: adds debug flag to commander

--- helpers/command_helper.js
+++ helpers/command_helper.js
@@ -4,7 +4,8 @@ const pkg = require('../package.json');
 
 program
   .version(pkg.version)
-  .option('--config <path>', 'define config path')
+  .option('--config <path>', 'set config path')
+  .option('--debug', 'debug flag to display debug in stdout')
   .parse(process.argv);
 
 export default program;
