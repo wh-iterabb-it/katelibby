8f6ef83b4fb163219b451efc5a16541e01028b10
Beau Bouchard <bnb2011@gmail.com>
2018-02-13 14:01:19 -0500

    feat: added chalk to logging outputs

--- utils/logger.js
+++ utils/logger.js
@@ -8,6 +8,49 @@ const logger = new (Logger)({
     new (transports.Console)({
       'timestamp': true,
       'level': level,
+      formatter: function (options) {
+        let message = "";
+        let meta = "";
+        
+        if (options.message !== undefined) {
+          message = options.message;
+        }
+
+        if (options.meta && Object.keys(options.meta).length) {
+          meta = "\n\t" + JSON.stringify(options.meta);
+        }
+        
+        const level = options.level.toUpperCase();
+
+        switch (level) {
+          case "DEBUG":
+            level = chalk.bgYellow(level);
+            break;
+
+          case "INFO":
+            level = chalk.cyan(level);
+            break;
+
+          case "WARN":
+            level = chalk.yellow(level);
+            break;
+
+          case "ERROR":
+            level = chalk.red(level);
+            break;
+
+          default:
+            break;
+        }
+
+        let output = [
+          `[${options.timestamp()}][${level}]`
+          message,
+          meta
+        ];
+
+        return output.join(' ');
+      }
     }),
   ],
 });
