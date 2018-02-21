6ca4be6a027f16d13f2f5a4ab8726d0b06620415
Beau Bouchard <bnb2011@gmail.com>
2018-02-21 11:21:16 -0500

    feat: added chalk to logging outputs (#137)
    * feat: added chalk to logging outputs
    
    * chore: adds chalk to deps
    
    * chore: fixed typo for array
    
    * chore: level was read only
    
    * chore: added chalk import
    
    * chore: prefer destructuring
    
    * fix: chalk format
    
    * fix: destructuring
    
    * fix: timestamp customization
    
    * feat: added dateformat
    
    * fix: added dateformat for usage

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,6 @@
 import { Logger, transports } from 'winston';
+import chalk from 'chalk';
+import dateformat from 'dateformat';
 import program from '../helpers/command_helper';
 
 const level = program.debug ? 'debug' : 'info';
@@ -6,8 +8,46 @@ const level = program.debug ? 'debug' : 'info';
 const logger = new (Logger)({
   transports: [
     new (transports.Console)({
-      'timestamp': true,
+      timestamp: function () {
+          return dateformat(Date.now(), "yyyy-mm-dd HH:MM:ss.l");
+      },
       'level': level,
+      formatter: function (options) {
+        let meta = '';
+
+        if (options.meta && Object.keys(options.meta).length) {
+          meta = '\n\t' + JSON.stringify(options.meta);
+        }
+        let formattedLevel = options.level.toUpperCase();
+        switch (formattedLevel) {
+          case 'DEBUG':
+            formattedLevel = chalk.bgYellow(formattedLevel);
+            break;
+
+          case 'INFO':
+            formattedLevel = chalk.cyan(formattedLevel);
+            break;
+
+          case 'WARN':
+            formattedLevel = chalk.yellow(formattedLevel);
+            break;
+
+          case 'ERROR':
+            formattedLevel = chalk.red(formattedLevel);
+            break;
+
+          default:
+            break;
+        }
+
+        let output = [
+          `[${options.timestamp()}][${formattedLevel}]`,
+          options.message,
+          meta
+        ];
+
+        return output.join(' ');
+      }
     }),
   ],
 });
