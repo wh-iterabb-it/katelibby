fab00ea5bbd771e4fc79f73e1abbbd7a724d38e9
beaubouchard <beau@beaubouchard.com>
2018-03-13 08:24:54 -0400

    fix: cleaned up, and base tests

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 import commands from './commands';
 import logger from './utils/logger';
-import xxmp from './helpers/irc_helper';
+// import xxmp from './helpers/irc_helper';
 import config from './helpers/config_helper';
 
 class App {
   static init() {
-    const { WebClient } = require('@slack/client');
-
-    // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
-    const token = process.env.SLACK_TOKEN;
-
-    const web = new WebClient(token);
-
-    // The first argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-    const channelId = 'C1232456';
-
-    // See: https://api.slack.com/methods/chat.postMessage
-    web.chat.postMessage(channelId, 'Hello there')
-      .then((res) => {
-        // `res` contains information about the posted message
-        console.log('Message sent: ', res.ts);
-      }).catch(console.error);
+    logger.info('taco bell');
+    logger.debug('taco bell debug');
+    logger.warn('taco bell warn');
+    // const { WebClient } = require('@slack/client');
+    //
+    // // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
+    // const token = process.env.SLACK_TOKEN;
+    //
+    // const web = new WebClient(token);
+    //
+    // // The first argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+    // const channelId = 'C1232456';
+    //
+    // // See: https://api.slack.com/methods/chat.postMessage
+    // web.chat.postMessage(channelId, 'Hello there')
+    //   .then((res) => {
+    //     // `res` contains information about the posted message
+    //     console.log('Message sent: ', res.ts);
+    //   }).catch(console.error);
   }
 
 }
---
+++ test/utils/logger.test.js
@@ -0,0 +1,24 @@
+import chai from 'chai';
+
+chai.should();
+
+const { expect } = chai;
+
+describe('Utils', () => {
+  let logger = require('../../utils/logger.js').default;
+  // stubbing out some tests for logger
+  describe('Logger', () => {
+    it('should log debug without errors', () => {
+      logger.debug('test');
+    });
+    it('should log info without errors', () => {
+      logger.info('test');
+    });
+    it('should log warn without errors', () => {
+      logger.warn('test');
+    });
+    it('should log error without errors', () => {
+      logger.error('test');
+    });
+  });
+});
