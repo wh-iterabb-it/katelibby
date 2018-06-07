ffa4f01147b143186d5efc443403c43b976c442e
Beau Bouchard <bnb2011@gmail.com>
2018-06-06 21:22:20 -0400

    release/1.0.2 (#260)
    * release/1.0.1 (#244) (#245)
    
    * feat: fortune command
    
    * fix: bracket
    
    * fix: removed reference to callback for nsfw
    
    * chore: added format and tests back
    
    * test: added test for help case
    
    * test: test for fortune help
    
    * chore: adding nsfw true
    
    * fix: help output expected corrected
    
    * chore: fixed help and added tests
    
    * fix: added eof break
    
    * feat: mtastatus command and sanitize
    
    * chore(package): update sinon-chai to version 3.1.0
    
    * chore(package): update sinon to version 5.0.8
    
    * chore(package): update sinon to version 5.0.10
    
    Closes #226
    
    * chore(package): update @babel/register to version 7.0.0-beta.48
    
    * chore(package): update @babel/core to version 7.0.0-beta.48
    
    * chore: base stats stuff
    
    * chore: adding comments
    
    * fix(package): update winston to version 3.0.0-rc6
    
    * Delete package-lock.json
    
    * chore(package): update nock to version 9.3.0
    
    * chore: created about command
    
    * chuck: a bunch of garbage
    
    * chore(package): update nyc to version 12.0.1
    
    * chore: more garbage
    
    * feat: created cat facts
    
    * fix: spacing
    
    * chore: stubbing out the feature
    
    * chore(package): update nyc to version 12.0.2
    
    * chore(package): update eslint-plugin-react to version 7.9.0
    
    * chore(package): update eslint-plugin-react to version 7.9.1
    
    * fix: missing config helper
    
    * chore(package): update tough-cookie to version 2.4.2
    
    * refactor: refactored to use a command factory
    
    * version bump for release
    
    * feat: added distribution (#248)
    
    * fix: removes annoying whitespaces (#250)
    
    * fix: removes annoying whitespaces
    
    * fix: moved it out of sanitize
    
    * Delete package-lock.json
    
    * fix(package): update @slack/client to version 4.3.0 (#251)
    
    * feat: added woot command (#257)
    
    * feat: added woot command
    
    * test: removed tests :P
    
    * test: added tests (#258)
    
    * test: added tests (#259)
    
    * feat: dockerfile (#252)
    
    * feat: dockerfile
    
    * fix: Travis touch-up
    
    * chore: docker-compose
    
    * chore: changed directory
    
    * feat: docker-compose file
    
    * test: added tests
    
    * chore(package): update sinon to version 5.1.0 (#253)

--- commands/mtastatus.js
+++ commands/mtastatus.js
@@ -14,6 +14,11 @@ const factoryParams = {
 };
 
 class MTA {
+
+  /**
+   * getLineKey
+   * @param {string} input - expected to be a single character for a MTA transit line
+   */
   static getLineKey(input) {
     switch (input.toUpperCase()) {
       case '1':
@@ -54,7 +59,11 @@ class MTA {
         return null;
     }
   }
-
+  
+  /**
+   * getColorForLine
+   * @param {string} input - returns the color for the line, this is normally used for IRC coloring
+   */
   static getColorForLine(line) {
     const lineColors = {
       '123': 'light_red',
@@ -97,13 +106,30 @@ const MtastatusCommand = function MtastatusCommand() {
               Sanitize.sanitize(striptags(currentLine.status));
             let outText = Sanitize.sanitize(striptags(currentLine.text));
             if (outText.length > 0) {
-              outStatus = outStatus + outText;
+              outStatus = outStatus + outText.replace(/\s+/g, ' ');
             }
             response = outStatus;
           }
         });
       });
       return Promise.resolve(response);
+    },
+
+    /**
+     * lineKey
+     * @param {string} input - expected to be a single character for a MTA transit line
+     */
+    lineKey: (input) => {
+      const output = MTA.getLineKey(input);
+      return output;
+    },
+
+    /**
+     * colorForLine
+     * @param {string} input - returns the color for the line, this is normally used for IRC coloring
+     */
+    colorForLine: (input) => {
+      return MTA.getColorForLine(input);
     },
   });
 };
