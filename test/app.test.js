580590b46d651b64168de06e641f5ce96943e530
Beau Bouchard <bnb2011@gmail.com>
2018-07-05 01:00:59 -0400

    chore: more test cases

--- test/app.test.js
+++ test/app.test.js
@@ -17,12 +17,27 @@ describe('App Tests', () => {
       kate.connectDiscord();
       // info log of 'No discord configuration found'
     });
+    it('should return expected log for valid config', () => {
+      config.discord = { token="stuff" };
+      kate.connectDiscord();
+    });
+  });
+  describe('connectSlack method', () => {
+    it('should return expected log for no config', () => {
+      config.slack = '';
+      kate.connectSlack();
+      // info log of 'No slack configuration found'
+    });
   });
   describe('connectIRC method', () => {
     it('should return expected log for no config', () => {
       config.irc = '';
       kate.connectIRC();
       // info log of 'No irc  configuration found'
+    });
+    it('should return expected log for a valid config', () => {
+      config.irc = { token="stuff" };
+      kate.connectIRC();
     });
   });
 });
