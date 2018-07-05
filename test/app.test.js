adabf3d183c759143c775da71fa4ee0f581cbd59
Beau Bouchard <bnb2011@gmail.com>
2018-07-05 01:18:36 -0400

    chore: added test cases for missing configs (#312)
    * chore: added test cases for missing configs
    
    * chore: more test cases
    
    * chore: typo
    
    * chore: fixing test

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 
+import config from '../helpers/config_helper';
 import kate from '../app';
 
 chai.should();
@@ -10,9 +11,39 @@ chai.use(sinonChai);
 const { expect } = chai;
 
 describe('App Tests', () => {
-  describe('setCommandPattern method', () => {
-    it('should return correct commandPattern when passed character', () => {
-
+  describe('connectDiscord method', () => {
+    it('should return expected log for no config', () => {
+      config.discord = '';
+      kate.connectDiscord();
+      // info log of 'No discord configuration found'
+    });
+    it('should return expected log for valid config', () => {
+      config.discord = [{
+        realName: 'Kate Libby',
+        token: 'tacobell', // REQUIRED
+      }];
+      kate.connectDiscord();
+    });
+  });
+  describe('connectSlack method', () => {
+    it('should return expected log for no config', () => {
+      config.slack = '';
+      kate.connectSlack();
+      // info log of 'No slack configuration found'
+    });
+  });
+  describe('connectIRC method', () => {
+    it('should return expected log for no config', () => {
+      config.irc = '';
+      kate.connectIRC();
+      // info log of 'No irc  configuration found'
+    });
+    it('should return expected log for a valid config', () => {
+      config.irc = [{
+        realName: 'Kate Libby',
+        token: 'tacobell', // REQUIRED
+      }];
+      kate.connectIRC();
     });
   });
 });
