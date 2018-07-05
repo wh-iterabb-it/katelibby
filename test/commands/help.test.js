0e181fbd155cceeba7339328e5c172182a135abe
Beau Bouchard <bnb2011@gmail.com>
2018-07-05 01:12:35 -0400

    chore: fixing test

--- test/commands/help.test.js
+++ test/commands/help.test.js
@@ -13,13 +13,20 @@ describe('Command', () => {
     const commandz = Object.keys(commandList).map((command) => {
       return config.commandChar + command;
     }).join(' ');
-
+    config.slack = [{
+      realName: 'Kate Libby',
+      token: 'tacobell', // REQUIRED
+    }];
     const msg = `Hello, I am ${config.slack[0].realName}, a bot. My commands are: `;
     const expected = `${msg}${commandz}\n\rYou can also type ${config.commandChar}{command} help, to get more info about a specific command`;
 
     const expectedHelp = `Syntax is ${config.commandChar}{command} help, to get more info about a specific command 🤔`;
 
     it('should return expected help result when invoked', (done) => {
+      config.slack = [{
+        realName: 'Kate Libby',
+        token: 'tacobell', // REQUIRED
+      }];
       try {
         commands.help.main('',config.slack[0]).then((result) => {
           expect(result).to.equal(expected);
